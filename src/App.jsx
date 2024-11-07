9ffd8cacc554007a24f908259d2a50f8f20266cd
Yazan Egbaria <yazanegbaria07@gmail.com>
2024-11-07 13:54:25 +0200

    Finished show and hide box task

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { useState } from "react";
+
 export default function App() {
-  return <div></div>;
+  const [isShown, setIsShown] = useState(false);
+  return (
+    <div className="mx-auto my-12 block w-fit text-center">
+      <button
+        onClick={() => {
+          setIsShown((prev) => !prev);
+        }}
+      >
+        {isShown ? "Hide Box" : "Show Box"}
+      </button>
+      {isShown && (
+        <div className="mx-auto mt-4 h-12 w-12 rounded bg-slate-600"></div>
+      )}
+    </div>
+  );
 }
