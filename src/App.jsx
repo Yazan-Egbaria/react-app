74f17c0510117339d91e1a27e02e59264ab7b689
Yazan Egbaria <yazanegbaria07@gmail.com>
2024-11-08 19:18:39 +0200

    Initilized menu project

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
 export default function App() {
-  return <div></div>;
+  return (
+    <div
+      id="mainContainer"
+      className="container mx-auto flex min-h-screen items-center justify-center bg-slate-100 py-10"
+    ></div>
+  );
 }
